aae00cb7baf364c8d6a4143008ca8816957fe37d
Maha Hossam <maha.mahmoudhamza@metronom.com>
2018-07-28 12:56:39 +0200

    Maha | GOTS-2 | Using Java8 Convension to enhance readability

--- src/main/java/com/devglan/config/SocketHandler.java
+++ src/main/java/com/devglan/config/SocketHandler.java
@@ -1,7 +1,6 @@
 package com.devglan.config;
 
 import com.google.gson.Gson;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -23,21 +22,24 @@ public class SocketHandler extends TextWebSocketHandler {
     private Boolean isFirstPlay = true;
 
     private String msg = "";
+    Integer gameNumber =0;
 
     @Override
     public void handleTextMessage(WebSocketSession session, TextMessage message)
             throws IOException {
 
         Map<String, String> value = new Gson().fromJson(message.getPayload(), Map.class);
-        //TODO
+
         if (isFirstPlay) {
-            Integer startOfGameNumber =
+             gameNumber =
                     value.get("number").length() > 0 ?
                             Integer.parseInt(value.get("number")) :
                             gameHelperGenerator.generateNumberDivisibleByThree();
 
-            msg = "Player " + session.getId() + " Placed: " + startOfGameNumber;
+            msg = "Player " + session.getId() + " Placed: " + gameNumber;
+            //added for history tracking
             messages.add(new TextMessage(msg));
+            //publish to add players
             for (WebSocketSession webSocketSession : sessions) {
                 webSocketSession.sendMessage(
                         new TextMessage(msg));
@@ -47,34 +49,28 @@ public class SocketHandler extends TextWebSocketHandler {
 
         }
 
-
-//        messages.add(new TextMessage(value.get("number") + " !" + "  " +
-//                session.getId()));
-//        for (WebSocketSession webSocketSession : sessions) {
-//            webSocketSession.sendMessage(
-//                    new TextMessage(value.get("number") + " !" + "  " +
-//                            session.getId())
-//
-//            );
-  //      }
     }
 
     @Override
-    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        //the messages will be broadcasted to all users.
+    public void afterConnectionEstablished(WebSocketSession session){
+        //the messages will be broadcasted to all users when they are connected
         sessions.add(session);
-        if (messages.size() > 0) {
-            for (TextMessage message : messages) {
-                session.sendMessage(message);
-            }
 
+        if (messages.size() > 0) {
+            messages.forEach(message -> {
+                try {
+                    session.sendMessage(message);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
         }
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
         sessions.remove(session);
-        if(sessions.size()==0)
+        if (sessions.size() == 0)
             messages.clear();
     }
 
